2112d9d841048ddf30d72f760910b6ddac52c7e1
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-06-30 22:57:54 +0300

    Add e2e test for opening an individual pokemon's page

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,13 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('individual pokemon pages can be accessed from the front page', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('ninetales').click()
+
+    await expect(page.getByText('ninetales')).toBeVisible()
+    await expect(page.getByText('flash fire')).toBeVisible()
+    await expect(page.getByText('drought')).toBeVisible()
+  })
 })
