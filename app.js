7a3eb68a681631172c1426e9fe32a2bb5b7605ac
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-07-01 14:19:58 +0300

    Break the build, add health check endpoint and use it in fly.io deployment

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
