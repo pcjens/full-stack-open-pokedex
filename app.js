c509c580e56968dcbf3438ce78c4c020f92b2d18
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-07-02 18:02:13 +0300

    Remove health check disabling endpoint, no longer needed

--- app.js
+++ app.js
@@ -10,19 +10,8 @@ app.get('/version', (req, res) => {
   res.send('3') // change this string to ensure a new version deployed
 })
 
-let healthStatus = 200
 app.get('/health', (req, res) => {
-  res.status(healthStatus).send(healthStatus === 200 ? 'ok' : 'uhh')
-})
-
-app.get('/health_stop', (req, res) => {
-  healthStatus = 500
-  res.send('/health should now return 500')
-})
-
-app.get('/health_start', (req, res) => {
-  healthStatus = 200
-  res.send('/health should now return 200')
+  res.send(200)
 })
 
 app.listen(PORT, () => {
